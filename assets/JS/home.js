6c26510c10d5f78e20f156eeb90c3a90be2a47e0
welersonfrr <contato.welerson@gmail.com>
2022-08-22 10:16:28 -0300

    Alteraçoes para compor ediçao de nota

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -32,6 +32,17 @@ function saveData(data) {
   return;
 }
 
+// Remover nota
+function removeNota(index) {
+  if (confirm("Deseja remover esta nota?")) {
+    logged.notes.splice(index, 1);
+    localStorage.setItem(logged.login, JSON.stringify(logged));
+    getNotas();
+    return;
+  }
+  return;
+}
+
 // Escrever notas na tela
 function getNotas() {
   const notas = logged.notes;
@@ -46,8 +57,8 @@ function getNotas() {
       <td>${e.detalhamento}</td>
       <td>
         <div class="action-group">
-          <button class="apagar" id="apagar-${index}">Apagar</button>
-          <button class="editar" id="editar-${index}">Editar</button>
+          <button class="apagar" id="apagar-${index}" onclick="removeNota(${index})"">Apagar</button>
+          <button class="editar" id="editar-${index}" onclick="modal(${index})"">Editar</button>
         </div>
       </td>
     </tr>
@@ -75,3 +86,48 @@ document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("session");
   window.location.href = "./index.html";
 });
+
+// escreve modal para ediçao da nota
+function modal(index) {
+  let htmlElement = ``;
+  htmlElement += `
+    <div class="modal">
+      <div class="edit-box">
+        <h3>Editar Nota</h3>
+          <div class="input-group">
+            <input type="text" value="${logged.notes[index].descricao}" id="newDescricao" required />
+            <input type="text" value="${logged.notes[index].detalhamento}" id="newDetalhamento" required />
+          </div>
+          <div class="action-group">
+            <button type="button" class="cancelar" id="cancelar" onclick="removeModal()">Cancelar</button>
+            <button type="submit" class="confirmar" id="confirmar" onclick="updateNota(${index})">Confirmar</button>
+          </div>
+      </div>
+    </div>
+  `;
+  document.getElementById("edit-form").innerHTML = htmlElement;
+  document.getElementById("edit-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+}
+
+// atualiza informação da nota
+function updateNota(index) {
+  let descricao = document.getElementById("newDescricao").value;
+  let detalhamento = document.getElementById("newDetalhamento").value;
+
+  logged.notes[index].descricao = descricao;
+  logged.notes[index].detalhamento = detalhamento;
+
+  localStorage.setItem(logged.login, JSON.stringify(logged));
+  alert("nota atualizada");
+
+  getNotas();
+  removeModal();
+  return;
+}
+
+// remove modal
+function removeModal() {
+  document.getElementById("edit-form").innerHTML = "";
+}
